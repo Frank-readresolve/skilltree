2bf34f51638fea85b8a31cd286e43ec828b3a791
Frank MARSHALL <frank@readresolve.io>
2024-06-13 11:38:11 +0200

    Fixed now zoneId

--- skilltree-business/src/main/java/tech/readresolve/skilltree/config/JwtProvider.java
+++ skilltree-business/src/main/java/tech/readresolve/skilltree/config/JwtProvider.java
@@ -9,20 +9,23 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator.Builder;
 import com.auth0.jwt.algorithms.Algorithm;
 
-public record JwtProvider(String issuer, long expiration, ZoneId zoneId,
-	Algorithm algorithm) {
+public record JwtProvider(String issuer, long expiration,
+	ZoneId zoneId, Algorithm algorithm) {
 
     String create(String subject, List<String> roles) {
-	LocalDateTime now = LocalDateTime.now();
+	LocalDateTime now = LocalDateTime.now(zoneId);
 	Instant issuedAt = now.atZone(zoneId).toInstant();
 	Builder builder = JWT.create().withIssuer(issuer)
 		.withIssuedAt(issuedAt);
 	if (expiration > -1) {
-	    LocalDateTime expires = now.plusSeconds(expiration);
-	    Instant expiresAt = expires.atZone(zoneId).toInstant();
+	    LocalDateTime expires = now
+		    .plusSeconds(expiration);
+	    Instant expiresAt = expires.atZone(zoneId)
+		    .toInstant();
 	    builder.withExpiresAt(expiresAt);
 	}
-	builder.withSubject(subject).withClaim("roles", roles);
+	builder.withSubject(subject).withClaim("roles",
+		roles);
 	return builder.sign(algorithm);
     }
 
