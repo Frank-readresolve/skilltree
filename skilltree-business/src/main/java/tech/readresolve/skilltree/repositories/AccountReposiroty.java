2e84eceaf544bca299a4edbf57a85969edb56479
Frank MARSHALL <frank@readresolve.io>
2025-08-12 09:29:26 +0200

    Sonar fix: indenting, replaced by tabs only

--- skilltree-business/src/main/java/tech/readresolve/skilltree/repositories/AccountReposiroty.java
+++ skilltree-business/src/main/java/tech/readresolve/skilltree/repositories/AccountReposiroty.java
@@ -12,16 +12,16 @@ import tech.readresolve.skilltree.entities.Account;
 @Repository
 public interface AccountReposiroty extends BaseRepository<Account> {
 
-    boolean existsByUsernameIgnoreCase(String value);
+	boolean existsByUsernameIgnoreCase(String value);
 
-    Optional<Account> findByUsernameIgnoreCase(String username);
+	Optional<Account> findByUsernameIgnoreCase(String username);
 
-    @Query("""
-    	select a.id as id, a.username as username, a.firstname as firstname,
-    		a.lastname as lastname, a.role.code as roleCode
-    			from Account a where a.username != :username
-    				order by a.lastname, a.firstname
-    	""")
-    Collection<AccountView> findAllProjectedBy(String username);
+	@Query("""
+			select a.id as id, a.username as username, a.firstname as firstname,
+				a.lastname as lastname, a.role.code as roleCode
+			from Account a where a.username != :username
+					order by a.lastname, a.firstname
+			""")
+	Collection<AccountView> findAllProjectedBy(String username);
 
 }
