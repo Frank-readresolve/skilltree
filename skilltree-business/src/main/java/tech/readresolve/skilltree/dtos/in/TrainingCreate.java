12f132e5fdd0f4b0214ac3e441079c49a5bb597f
Frank MARSHALL <frank@readresolve.io>
2025-08-14 13:57:58 +0200

    Refactored training dates validation, updated related tests

--- skilltree-business/src/main/java/tech/readresolve/skilltree/dtos/in/TrainingCreate.java
+++ skilltree-business/src/main/java/tech/readresolve/skilltree/dtos/in/TrainingCreate.java
@@ -5,13 +5,12 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import tech.readresolve.skilltree.dtos.in.constraints.TrainingDatesConsistent;
 import tech.readresolve.skilltree.dtos.in.constraints.TrainingUniqueName;
 
 @TrainingDatesConsistent
 public record TrainingCreate(@NotNull Long certificationId,
 	@NotBlank @Size(min = 1, max = 150) @TrainingUniqueName String name,
-	@NotNull LocalDate startDate, @NotNull LocalDate endDate,
+	LocalDate startDate, LocalDate endDate,
 	@NotBlank @Size(min = 1, max = 2000) String description) {
 }
--- skilltree-business/src/main/java/tech/readresolve/skilltree/dtos/in/constraints/validators/TrainingDatesConsistentValidator.java
+++ skilltree-business/src/main/java/tech/readresolve/skilltree/dtos/in/constraints/validators/TrainingDatesConsistentValidator.java
@@ -3,7 +3,6 @@ package tech.readresolve.skilltree.dtos.in.constraints.validators;
 import java.time.LocalDate;
 
 import jakarta.validation.ConstraintValidatorContext;
-
 import tech.readresolve.skilltree.dtos.in.TrainingCreate;
 import tech.readresolve.skilltree.dtos.in.constraints.TrainingDatesConsistent;
 
@@ -15,8 +14,8 @@ public final class TrainingDatesConsistentValidator extends
 	    ConstraintValidatorContext context) {
 	LocalDate startDate = value.startDate();
 	LocalDate endDate = value.endDate();
-	if (startDate == null || endDate == null) {
-	    return true;
+	if (startDate == null || endDate == null) { // Not comparable
+	    return false;
 	}
 	return endDate.isAfter(startDate);
     }
